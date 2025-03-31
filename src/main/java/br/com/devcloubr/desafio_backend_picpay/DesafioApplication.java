88df8436e7c539e8d3c48d334cdb3a35f7b45033
adenilson ne <adenilson.ini@gmail.com>
2025-03-31 14:41:31 -0300

    commit inicial - criação do projeto

---
+++ src/main/java/br/com/devcloubr/desafio_backend_picpay/DesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.devcloubr.desafio_backend_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+}
